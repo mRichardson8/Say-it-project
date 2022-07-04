c61be33b90f422cd7b6dd933c4fe1bc4d1735dd0
Matthew Richardson <matt.rich2045@gmail.com>
2022-07-04 12:30:06 +0100

    Added more function skeletons and eventListener for the from POST request

--- client/assets/index.js
+++ client/assets/index.js
@@ -4,7 +4,7 @@ let testObj = [
     title: "Why not",
     post: "Why not take a crazy chance, why not do a crazy dance!",
     reactions: [1,2,3],
-    reply   : ["I love Hilary Duff", "I hate Hilary Duff", "I think Hilary Duff is just right"]
+    reply   : ["I love Hilary Duff", "I hate Hilary Duff", "I think Hilary Duff is just alright"]
 },
 {
     title: "Just can't get enough",
@@ -17,7 +17,7 @@ let testObj = [
 //! This function runs when the window first loads and retrieves all the posts from the server. For each post retrieved, the createPost function is called passing in that post as an argument
 async function getPosts(){
     try{
-        let response = await fetch('/')
+        let response = await fetch('localhost:3000/')
         let data = await response.json()
         for (let i = 0; i < data.length; i++) {
             createPost(data[i])
@@ -44,16 +44,60 @@ function createPost(data){
     let text = document.createElement('p');
     text.innerText = data.post;
     let reactions = document.createElement('div');
-    // add function to populate the reactions div
+    // let reactions = createReactions(data.reactions)
     let replies = document.createElement('div');
-    //add function to populate the replies div
+    // let replies = createReplies(data.replies)
     postBox.append(title, text, reactions, replies)
     postList.appendChild(postBox)
 }
 
+function createReactions(reactArr){
+    let reactions = document.createElement('div')
+    //TODO read from the array of reactions and create a div populated with the reaction emojis and their respective numbers
+    return reactions
+}
+
+function createReplies(repliesArr){
+    let replies = document.createElement('div')
+    //TODO read from the array of replies and create a div populated with each string as a separate reply
+    return replies
+}
+
+function submitPost(e){
+    return
+}
 //event listeners
 
+// add a new div to handle the gif keyboard, this dif starts with display set to none but when button is clicked it becomes flex
+let gifBtn = document.getElementById('gif-btn')
+gifBtn.addEventListener('click', (e) => {
+    let gifDiv = document.getElementById('gif-cont')
+    gifDiv.style.display = 'flex'
+});
 
+//details from new blog post are sent as a post request to the server. If successful the page reloads
+let newPostForm = document.getElementById('new-post-form')
+newPostForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    let postTitle = "title" // document.getElementById('post-title').value //TODO need to add a text input for title in the form
+    let postBody = document.getElementById('form-text').value
+    let postGif = "" //document.getElementById('post-img').src //TODO need to add blank img into the form details
+    let response = await fetch('localhost:3000/new', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title: postTitle, post: postBody, img: postGif})
+    })
+    if (response.status(204)){ 
+        location.reload()
+    } else{
+        console.log("Huge error")
+        console.log(response.body)
+    }
+    
+})
 
 //Run the setup
 getTestPosts()
