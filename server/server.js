acd769fc482eaacbdb0f02fe1558d415875369ea
Matthew Richardson <matt.rich2045@gmail.com>
2022-07-05 17:27:50 +0100

    Changed port to PORT for deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const Blog = require('./models/blog');
 
 
